93f8d5a455f1028b2bf86799fe3f44f57f99d04c
Alejandro Franco Silva <afrasilv@gmail.com>
2016-07-07 00:06:24 +0200

    + Added TwitterLogin

---
+++ app/src/main/java/com/afrasilv/twitterclientedxcourse/main/MainActivity.java
@@ -0,0 +1,15 @@
+package com.afrasilv.twitterclientedxcourse.main;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.afrasilv.twitterclientedxcourse.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
